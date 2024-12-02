9ca751973386f827ebffb6fb64ca11f07d64c09a
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-12-02 19:46:04 +0530

    new better

--- background.js
+++ background.js
@@ -2,14 +2,12 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && tab.active) {
     const url = tab.url;
 
-    if (url.startsWith("https://www.youtube.com/shorts/") || url.startsWith("stagram.com/reels/") ) {
-      // Check if the extension is active
-      chrome.storage.sync.get("isActive", (data) => {
-        if (data.isActive) {
-          chrome.tabs.update(tabId, { url: "https://github.com" });
-          
-        }
-      });
+    if (url.startsWith("https://www.youtube.com/shorts/") || url.startsWith("instagram.com/reels/") ) {
+
+      console.log(url);
+      chrome.tabs.update(tabId, { url: "https://github.com" });
+
+      
     }
   }
 });
